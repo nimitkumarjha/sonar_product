176e4bd77f616f38a1db7a1fb487b378ae591a97
nimitkumarjha <65605059+nimitkumarjha@users.noreply.github.com>
2020-06-21 17:46:52 +0530

    Add files via upload

---
+++ src/main/java/com/OMS/ProductService/controller/ProductController.java
@@ -0,0 +1,90 @@
+package com.OMS.ProductService.controller;
+
+
+
+//import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+//import javax.persistence.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.OMS.ProductService.dto.StockDTO;
+import com.OMS.ProductService.dto.productDTO;
+//import com.OMS.ProductService.entity.product;
+import com.OMS.ProductService.service.productService;
+
+@RestController
+@EnableAutoConfiguration
+@CrossOrigin
+@RequestMapping("api/")
+public class ProductController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	productService proService;
+	
+	@Autowired
+	private Environment en;
+	
+		
+	// Fetches all product details
+	@GetMapping(value = "/product",  produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<productDTO> getallproduct() {
+		return proService.getallproducts();
+	}
+
+	//Search product based on category 	
+	@GetMapping(value ="/product/category/{category}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<productDTO> getproduct(@PathVariable("category") String category){
+		List<productDTO> specificproducts=null;
+		specificproducts=proService.getSpecificproducts(category);
+		return specificproducts;
+	}
+
+	//Addition of new Product
+	@PostMapping(value = "/product",  consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> newproduct(@RequestBody productDTO prodDTO) {
+		ResponseEntity<String> responseEntity = null;
+		logger.info("Creation request for new product {}", prodDTO);
+		//System.out.println("=========Addition of newproduct- I am in Controller=========");
+		proService.newproduct(prodDTO);
+		String successMessage =en.getProperty("Product added successfully");
+		responseEntity = new ResponseEntity<String>(successMessage, HttpStatus.OK);
+		return responseEntity;
+	}
+	
+	//Updation of stock in existing product
+	@PutMapping(value = "/product/{prodid}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String updateproduct(@PathVariable("prodid") Integer prodid,@Valid @RequestBody StockDTO stockcontrol ) {
+		
+		logger.info("Creation request for updation of existing product {}",stockcontrol);
+		//System.out.println("=========updating existing product- I am in Controller=========");
+		proService.updateproduct(prodid, stockcontrol);
+		return "Pass controller";
+		
+		
+	}
+	//
+	
+
+	
+}
---
+++ src/main/java/com/OMS/ProductService/repository/productRepository.java
@@ -0,0 +1,26 @@
+package com.OMS.ProductService.repository;
+
+
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.OMS.ProductService.entity.product;
+
+
+public interface productRepository extends JpaRepository<product, Integer> {
+
+	List<product> getByCategory(String category);
+
+	//List<product> findAllById(Integer prodid);
+	
+	
+		
+
+   
+
+}
